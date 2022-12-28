63aa1242352a2ae8a98f5455e5eee3737f67c963
FedDev <dardo.bourgez@gmail.com>
2022-12-28 20:23:08 -0300

    Añadimos la funcionalidad del checkmark y su persistencia en el local storage

--- components/addTask.js
+++ components/addTask.js
@@ -1,3 +1,4 @@
+import { uniqueDates } from '../services/date.js';
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 import { displayTasks } from './readTasks.js';
@@ -18,9 +19,13 @@ export const addTask = (evento) => {
     input.value = '';
     calendar.value="";
 
+    const complete = false;
+
     const taskObj = {
       value, 
-      dateFormat
+      dateFormat,
+      complete,
+      id: uuid.v4()
     };    
   
     list.innerHTML = "";
@@ -32,16 +37,23 @@ export const addTask = (evento) => {
     displayTasks();
   }
   
-  export const createTask = ({value, dateFormat}) => {      
+  export const createTask = ({value, dateFormat, complete, id}) => {      
     const task = document.createElement('li');
           task.classList.add('card');    
     const taskContent = document.createElement('div');
     
-  
+      console.log(complete);
+      const check = checkComplete(id);
+      if(complete){
+            console.log("Completada");
+            check.classList.toggle('fas');
+            check.classList.toggle('completeIcon');
+            check.classList.toggle('far');
+      }
     const titleTask = document.createElement('span');
           titleTask.classList.add('task');
           titleTask.innerText = value;
-          taskContent.appendChild(checkComplete());
+          taskContent.appendChild(check);
           taskContent.appendChild(titleTask);
 
     const dateElement = document.createElement("span");
